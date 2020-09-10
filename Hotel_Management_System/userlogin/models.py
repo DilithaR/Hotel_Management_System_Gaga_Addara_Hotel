bc7d6daa5367454f03370f72ecbe7b814623a53d
MahendraThammita <dhmmpthammita@gmail.com>
2020-09-10 22:54:37 +0530

    Compleated for 1st evaluation

--- Hotel_Management_System/userlogin/models.py
+++ Hotel_Management_System/userlogin/models.py
@@ -11,11 +11,15 @@ import random
 
 class Employee(models.Model):
     # Field name made lowercase.
-    empid = models.CharField(db_column='empId', primary_key=True, max_length=6)
+    emp_index = models.AutoField(db_column='emp_Index', primary_key=True)
     # Field name made lowercase.
-    empnic = models.CharField(db_column='empNIC', max_length=10)
+    empid = models.CharField(db_column='empId', max_length=6)
     # Field name made lowercase.
-    faceid = models.CharField(db_column='faceID', max_length=500)
+    empnic = models.CharField(
+        db_column='empNIC', max_length=10, blank=True, null=True)
+    # Field name made lowercase.
+    faceid = models.CharField(
+        db_column='faceID', max_length=500, blank=True, null=True)
     # Field name made lowercase.
     f_name = models.CharField(
         db_column='f_Name', max_length=10, blank=True, null=True)
@@ -43,31 +47,36 @@ class Employee(models.Model):
     remark = models.CharField(max_length=500, blank=True, null=True)
     rating = models.IntegerField(blank=True, null=True)
     gender = models.CharField(max_length=6, blank=True, null=True)
+    last_login = models.CharField(max_length=150, blank=True, null=True)
 
     class Meta:
-        managed = True
+        managed = False
         db_table = 'Employee'
 
 
 class Admin(models.Model):
     # Field name made lowercase.
-    adminid = models.CharField(
-        db_column='adminId', primary_key=True, max_length=6)
+    emp_index = models.AutoField(db_column='emp_Index', primary_key=True)
     # Field name made lowercase.
-    empid = models.ForeignKey(Employee, models.DO_NOTHING, db_column='empId')
+    adminid = models.CharField(db_column='adminId', max_length=6)
+    # Field name made lowercase.
+    empid = models.CharField(db_column='empId', max_length=6)
     password = models.CharField(max_length=10, blank=True, null=True)
+    last_login = models.CharField(max_length=150, blank=True, null=True)
 
     class Meta:
-        managed = True
+        managed = False
         db_table = 'admin'
-        unique_together = (('adminid', 'empid'),)
 
 
 class Customer(models.Model):
     # Field name made lowercase.
-    cusid = models.CharField(db_column='cusId', primary_key=True, max_length=8)
+    cus_index = models.AutoField(db_column='cus_Index', primary_key=True)
     # Field name made lowercase.
-    cusnic = models.CharField(db_column='cusNIC', max_length=10)
+    cusid = models.CharField(db_column='cusId', max_length=8)
+    # Field name made lowercase.
+    cusnic = models.CharField(
+        db_column='cusNIC', max_length=10, blank=True, null=True)
     # Field name made lowercase.
     f_name = models.CharField(
         db_column='f_Name', max_length=10, blank=True, null=True)
@@ -81,32 +90,17 @@ class Customer(models.Model):
     address_l2 = models.CharField(
         db_column='address_L2', max_length=30, blank=True, null=True)
     postcode = models.IntegerField(blank=True, null=True)
-    img = models.CharField(max_length=250, blank=True, null=True)
+    img = models.ImageField(null = True , blank = True)
     # Field name made lowercase.
     email = models.CharField(
         db_column='Email', max_length=150, blank=True, null=True)
     gender = models.CharField(max_length=6, blank=True, null=True)
     password = models.CharField(max_length=10, blank=True, null=True)
-    phone = models.CharField(max_length=15, blank=True, null=True)
+    last_login = models.CharField(max_length=150, blank=True, null=True)
 
     class Meta:
-        managed = True
+        managed = False
         db_table = 'customer'
-
-    # def __init__(self, cusid, cusnic, f_name, l_name, address_l1, address_l2, postcode, img, email, gender, password, phone):
-
-    #     self.cusid = cusid
-    #     self.cusnic = cusnic
-    #     self.f_name = f_name
-    #     self.l_name = l_name
-    #     self.address_l1 = address_l1
-    #     self.address_l2 = address_l2
-    #     self.postcode = postcode
-    #     self.img = img
-    #     self.email = email
-    #     self.gender = gender
-    #     self.password = password
-    #     self.phone = phone
 
 
 class generateRandomeNum:
--- Hotel_Management_System/userlogin/views.py
+++ Hotel_Management_System/userlogin/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Customer , Admin
+from .models import Customer , Admin , Employee
 from .models import generateRandomeNum
 from django.contrib.auth.models import auth
 from django.shortcuts import redirect
+from .forms import custmoneForm
 
 # Create your views here.
 
@@ -13,10 +14,46 @@ def home(request):
 
 def login(request):
     return render(request, 'login.html')
+    
+
+def signUp(request):
+    return render(request, 'Signup.html')
 
 
 def admin(request):
     return render(request, 'Dashboard.html')
+
+
+def forgetpassword(request):
+    return render(request, 'Forgot_password_1.html')
+
+
+def forgetpw2(request):
+    return render(request, 'Forgot_password_2.html')
+
+
+def fogetPw3(request):
+    return render(request, 'Forgot_password_3.html')
+
+
+def customerLogout(request):
+    return render(request, 'login.html')
+
+
+def addEmployees(request):
+    return render(request, 'AddEmployee.html')
+
+
+def employeerepo(request):
+    return render(request, 'AttendenceReport.html')
+
+    
+
+
+def myprofile(request):
+    print("sesseionvalue : " + request.session['userid'])
+    cus1 = Customer.objects.get(cusid=request.session['userid'])
+    return render(request, 'User_profile.html' , {'customer' : cus1} )
 
 # login function
 def vertifyLogin(request):
@@ -26,34 +63,78 @@ def vertifyLogin(request):
 
     print(Password)
 
-    customer = Customer()
-    admin = Admin()
+    # admin = Admin.objects.get(adminid=Email)
+    # if admin is not None:
+    #     #auth.login(request, customer)
+    #     if admin.password == Password:
+    #         request.session['userid'] = admin.adminid
+    #         print("Customer valid")
+    #         return render(request, 'Dashboard.html', {'userid': admin.adminid})
+    #     else:
+    #         request.session['userid'] = None
+    #         return render(request, 'Dashboard.html')
+    # else:
+    #     customer = Customer.objects.get(email=Email)
+    #     if customer is not None:
+    #         if customer.password == Password:
+    #             request.session['eid'] = customer.adminid
+    #             print("customer valid")
+    #             return render(request, 'index.html', {'userid': customer.cusid, 'userEmail': customer.email})
+    #     else:
+    #         request.session['userid'] = None
+    #         return redirect('login')
 
-    customer = auth.authenticate(email=Email, password = Password)
-    print("cus checked")
+
+
+    #-----------------------------------------------
+
+    
+    customer = Customer.objects.get(email=Email)
+
+
     if customer is not None:
-        auth.login(request, customer)
-        print("Customer")
-        return redirect("admin")
-        
+        #auth.login(request, customer)
+        if customer.password == Password:
+            request.session['userid'] = customer.cusid
+            print("Customer valid")
+            return render(request, 'index.html', {'userid': customer.cusid , 'userEmail' : customer.email})
+        else:
+            request.session['userid'] = None
+            return redirect('login')
     else:
-        admin = auth.authenticate(adminid=Email, password = Password)
-        print("add checked")
+        admin = Admin.objects.get(adminid=Email)
         if admin is not None:
-            auth.login(request, admin)
-            print("Customer")
-            return redirect('admin')
+            if admin.password == Password:
+                request.session['eid'] = admin.adminid
+                print("admin valid")
+                return render(request, 'Dashboard.html')
+        else:
+            request.session['userid'] = None
+            return redirect('login')
+    
 
-    print("function skiped")
-    return render(request, 'login.html')
+
+
+
+   
+
+            
+    #     admin = auth.authenticate(adminid=Email, password = Password)
+    #     print("add checked")
+    #     if admin is not None:
+    #     auth.login(request, admin)
+        
+
+    # print("function skiped")
+    # return render(request, 'login.html')
 
 # Signup function
-def signUp(request):
+def signUpVer(request):
 
     rand = generateRandomeNum() 
 
 
-    custId = 'CUS' + rand.fiveNums()
+    custId = "CUS" + rand.fiveNums()
     print("custId" + custId)
     Email = request.POST['signEmail']
     Password = request.POST['signPW']
@@ -64,11 +145,11 @@ def signUp(request):
     if ConPw != Password:
         return render(request, 'login.html')
 
-    elif Customer.objects.filter(email=Email).exists:
-        return render(request, 'login.html')
+    # elif Customer.objects.filter(email=Email).exists:
+    #     return render(request, 'login.html')
 
-    elif Customer.objects.filter(cusid=custId).exists:
-        return render(request, 'login.html')
+    # elif Customer.objects.filter(cusid=custId).exists:
+    #     return render(request, 'login.html')
 
     else:
         customer = Customer(cusid=custId, email=Email,
@@ -78,7 +159,91 @@ def signUp(request):
         return render(request, 'index.html')
 
 
+def updateCus(request , id_cus):
+    cus = Customer.objects.get(cus_index = id_cus)
+    print(id_cus)
+    Email = request.POST['Edit_Email']
+    Nic = request.POST['Edit_Nic']
+    line1 = request.POST['AddressLineOne']
+    line2 = request.POST['Edit_AddLine2']
+    pCode = request.POST['Edit_PCode']
+    fname = request.POST['Edit_Fname']
+    lname = request.POST['Edit_Lname']
+    print("cusname " + lname)
     
+    cus2 = Customer(cus_index=id_cus , f_name=fname, l_name=lname,  email=Email,
+        address_l1=line1, address_l2=line2, postcode=pCode, cusnic=Nic , )
+    cus.save()
+    cus1 = Customer.objects.get(cusid=request.session['userid'])
+    return render(request, 'User_profile.html', {'customer': cus2})
+    # form = custmoneForm(request.POST , instance = cus)
+    # if form.is_valid:
+    #     form.save()
+    #     cus1 = Customer.objects.get(cusid=request.session['userid'])
+    #     return render(request, 'User_profile.html', {'customer': cus1})
+
+
+def adduser(request):
+
+    rand = generateRandomeNum()
+
+    addEmpEID = "EMP" + rand.fiveNums()
+    print("custId" + addEmpEID)
+
+    addEmpFname = request.GET['addEmpFname']
+    addEmpLname = request.GET['addEmpLname']
+    addEmpNIC = request.GET['addEmpNIC']
+    #addEmpMale = request.GET['addEmpMale']
+    addEmpEmail = request.GET['addEmpEmail']
+
+    addEmpPhone = request.GET['addEmpPhone']
+    addEpLineOne = request.GET['addEpLineOne']
+    addEpLineTwo = request.GET['addEpLineTwo']
+    addEmpcity = request.GET['addEmpcity']
+    addEmpPCode = request.GET['addEmpPCode']
+    addempDateOfJoin = request.GET['addempDateOfJoin']
+    addempSal = request.GET['addempSal']
+    addempOTRate = request.GET['addempOTRate']
+
+    employee = Employee(empid=addEmpEID, f_name=addEmpFname,
+                        l_name=addEmpLname, empnic=addEmpNIC, gender='Male',    email=addEmpEmail, phone=addEmpPhone,
+                        address_l1=addEpLineOne, address_l2=addEpLineTwo, postcode=addEmpPCode, reg_date=addempDateOfJoin, basic_sal=addempSal,
+                        ot_rate=addempOTRate)
+    employee.save()
+    print("Saved Customer")
+    emp = Employee.objects.all()
+    return render(request, 'EmployeeList.html', {'employees': emp})
 
 
 
+
+
+def fullemployee(request, id_emp):
+    emp = Employee.objects.get(emp_index=id_emp)
+    return render(request, 'ViewEmployee.html', {'employee': emp})
+
+
+def editemp(request):
+
+    #eid = request.GET['Edit_eid']
+    addEmpFname = request.GET['Edit_E_Fname']
+    addEmpLname = request.GET['Edit_E_Lname']
+    addEmpNIC = request.GET['Edit_E_nic']
+    #addEmpMale = request.GET['addEmpMale']
+    addEmpEmail = request.GET['Edit_E_email']
+
+    addEmpPhone = request.GET['Edit_E_phone']
+    addEpLineOne = request.GET['Edit_E_assL1']
+    addEpLineTwo = request.GET['Edit_E_assL2']
+    addEmpcity = request.GET['Edit_E_city']
+    addEmpPCode = request.GET['Edit_Epost_code']
+    #addempDateOfJoin = request.GET['Edit_Appdate']
+    addempSal = request.GET['Edit_Bsal']
+    addempOTRate = request.GET['Edit_OTrate']
+
+    employee = Employee(empid='EMP98245', f_name=addEmpFname,
+                        l_name=addEmpLname, empnic=addEmpNIC, gender='Male',    email=addEmpEmail, phone=addEmpPhone,
+                        address_l1=addEpLineOne, address_l2=addEpLineTwo, postcode=addEmpPCode, basic_sal=addempSal,
+                        ot_rate=addempOTRate)
+
+    return render(request, 'ViewEmployee.html', {'employee': employee})
