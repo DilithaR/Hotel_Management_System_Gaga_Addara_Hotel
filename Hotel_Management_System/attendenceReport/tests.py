4dc31e5fbcbe7b965647e1a35041931974075f13
MahendraThammita <dhmmpthammita@gmail.com>
2020-10-23 23:32:58 +0530

    test cases added

--- Hotel_Management_System/attendenceReport/tests.py
+++ Hotel_Management_System/attendenceReport/tests.py
@@ -1,3 +1,99 @@
-from django.test import TestCase
-
+from django.test import TestCase, SimpleTestCase, Client
+from django.urls import reverse
+from userlogin.models import Employee
+from faceRecognitionAttendence.models import Attendencesheet
+import json
 # Create your tests here.
+empCountList[] = ''
+
+class viewTestForReport(TestCase):
+
+    
+
+    def setEmployee(self):
+        self.client = Client()
+        self.HtmlReportUrl = reverse('attendenceReport')
+        self.EMP00001 = Employee.objects.create(
+            empid="EMP00001",
+            empnic="586945788V",
+            faceid=None,
+            f_name="Mahen",
+            l_name="Perera",
+            address_l1="No 14/34A",
+            address_l2="Colombo 08",
+            postcode=22458,
+            basic_sal=50000.00,
+            ot_rate=250.00,
+            reg_date='2020-10-12',
+            email="mahenperera@gmail.com",
+            phone='0716589258',
+            remark=None,
+            rating=4,
+            gender="Male",
+            occu="Waiter",
+        )
+
+    def testDailyEmpAttendence(self):
+        response = self.client.get(self.HtmlReportUrl)
+
+        sampleAttendence = Attendencesheet.objects.create(
+            empid=self.EMP00001,
+            date='2020-10-20',
+            timein='08:22:45',
+            timeout='16:22:45',
+            noofhours=None,
+            status='Compleated'
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(empCountList.count(), 1)
+        self.assertEqual(empCountList, 18)
+        self.assertEqual(sampleAttendence.noofhours,  '08.00')
+
+    def testEmpSalCalculation(self):
+        response = self.client.get(self.HtmlReportUrl)
+
+        sampleAttendence_1 = Attendencesheet.objects.create(
+            empid=self.EMP00001,
+            date='2020-10-20',
+            timein='08:22:45',
+            timeout='16:22:45',
+            noofhours=None,
+            status='Compleated'
+        )
+        sampleAttendence_2 = Attendencesheet.objects.create(
+            empid=self.EMP00001,
+            date='2020-10-20',
+            timein='08:22:45',
+            timeout='16:22:45',
+            noofhours=None,
+            status='Compleated'
+        )
+        sampleAttendence_3 = Attendencesheet.objects.create(
+            empid=self.EMP00001,
+            date='2020-10-20',
+            timein='08:22:45',
+            timeout='16:22:45',
+            noofhours=None,
+            status='Compleated'
+        )
+
+        url = reverse('CalcSalary')
+
+        response = self.client.get(url , {
+            'ExtraAllowneces' : 6000.00
+        })
+
+
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(sampleAttendence, '08.00')
+        self.assertEqual(empCountList.count(), 1)
+        self.assertEqual(expected_Days, 18)
+
+    def testAttendenceBasics(self):
+        client = Client()
+        response = client.get(reverse('attendenceReport'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'AttendenceReport.html')
--- Hotel_Management_System/userlogin/tests.py
+++ Hotel_Management_System/userlogin/tests.py
@@ -1,3 +1,75 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase, Client
+from django.urls import reverse
+from userlogin.models import Employee , Customer
+from faceRecognitionAttendence.models import Attendencesheet
+import json
 
 # Create your tests here.
+
+
+def setCustoomer(self):
+    self.client = Client()
+    self.EditUserURl = reverse('updateCus')
+    self.CUS00001 = Employee.objects.create(
+        cusid= 'CUS00001',
+        cusnic= '782596458V',
+        f_name= 'Nuwan',
+        l_name= 'Kulasekara' , 
+        address_l1= 'No 2/A258',
+        address_l2= 'Piliyandala',
+        postcode= 22598,
+        email= 'nuwankulasekara@gmail.com',
+        gender= 'Male',
+        password= '123ABC'
+    )
+
+def testUpdateCustomer(self):
+
+    self.EditUserURl = reverse('updateCus')
+
+    response = self.client.get(EditUserURl , {
+        
+        'first_Name'='Saman',
+        'last_Name'='Thilakarathne',
+        "address_line_1"='No 6/259B',
+        "address_line_2"='Kesbewa',
+        "postcode"=22569,
+        'u_email'='samathilakarathne@gmail.com',
+        'gender'='Male',
+
+    })
+
+    self.assertEqual(response.status_code, 302)
+    self.assertEqual(self.CUS00001.cusnic, 'Saman')
+    self.assertEqual(self.CUS00001.l_name, 'Thilakarathne')
+    self.assertEqual(self.CUS00001.address_l1, 'No 6/259B')
+    self.assertEqual(self.CUS00001.address_l2, 'Kesbewa')
+    self.assertEqual(self.CUS00001.postcode, 22569)
+    self.assertEqual(self.CUS00001.email, 'samathilakarathne@gmail.com')
+
+
+def testResetUserPassword(self):
+    self.checkEmailForresetPwURl = reverse('forgetpassword')
+
+    response = self.client.get(checkEmailForresetPwURl, {
+
+        'CheckEmail'='samathilakarathne@gmail.com',
+    })
+
+    testUpdatePassword()
+    self.assertEqual(response.status_code, 302)
+
+
+def testUpdatePassword(self):
+    self.resetPwURl = reverse('fogetPw3')
+    response = self.client.get(checkEmailForresetPwURl, {
+
+        'newPassword'='QAZ111',
+    })
+
+    self.assertEqual(response.status_code, 302)
+    self.assertEqual(self.CUS00001.password, 'QAZ111')
+    self.assertEqual(self.CUS00001.email, 'samathilakarathne@gmail.com')
+
+
+
